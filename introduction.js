fff22abc0d5e9f1c0d391a53456946037a6f73b5
vivajane <obuekwekosi@gmail.com>
2022-06-24 15:49:52 +0100

    first commit

---
+++ introduction.js
@@ -0,0 +1,11 @@
+var firstName =  'Ojiteli';
+var middleName = 'kosisochukwu';
+var lastName = 'Vivian';
+var height = '5.6ft';
+var country = 'Nigeria';
+
+name = firstName + ' ' + middleName + ' ' + lastName;
+
+document.write('Name:' + ' '+ name + ' ' +  '<br/>');
+document.write('Height:' + ' ' + height + '<br/>');
+document.write('Country:' + ' ' + country + '<br/>');
